8f695f51acdd735a5d3bab176bd04f9a35afa182
kim75503 <kim755030@naver.com>
2025-12-23 15:04:19 +0900

    test1

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("NHN Academy Step 1 Start");
+        System.out.println("NHN Academy Step 1.0 Start");
     }
 }
